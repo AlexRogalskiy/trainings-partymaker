b4e161c35400b107e07c0204a7e2292171e8783e
Mstislav Martynyuk <mmartynyuk@reunico.com>
2021-04-09 13:12:54 +0300

    CORS disabled

---
+++ src/main/java/com/reunico/bpm/conf/CorsDisable.java
@@ -0,0 +1,22 @@
+package com.reunico.bpm.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Component
+public class CorsDisable {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
