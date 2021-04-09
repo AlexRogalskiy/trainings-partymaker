e10e99544d0554082947eed92d8f92bf2c7a1e3f
Mstislav Martynyuk <mmartynyuk@reunico.com>
2021-04-09 13:32:54 +0300

    CORS disabled

--- src/main/java/com/reunico/bpm/conf/CorsDisable.java
+++ src/main/java/com/reunico/bpm/conf/CorsDisable.java
@@ -1,12 +1,15 @@
 package com.reunico.bpm.conf;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-@Component
+@Configuration
 public class CorsDisable {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -19,4 +22,20 @@ public class CorsDisable {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+    @Bean
+    public FilterRegistrationBean processCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+
 }
